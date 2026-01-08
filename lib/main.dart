d09a8892abcec9063283ae6bf092b48180e2d039
MaximCapcanari <mcapcanari@gmail.com>
2026-01-08 10:42:39 -0800

    no debug tag

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Available BLE devices',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
